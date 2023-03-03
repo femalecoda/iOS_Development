b353c85bef360056171c71f7fd5d7da2b9f41038
Zainab Odia <odiazainab@gmail.com>
2023-03-03 19:54:58 +0100

    Initial Commit

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,81 @@
+
+// keyword: var is modifiable or Mutable
+// keyword: let is constant ot Immutable
+
+var  age = 20     // implicity declaration
+
+var lenght: Int = 21  // explicity declaration
+
+var roomLenght: Double = 25.8
+
+var tempreture = 35.9
+
+
+
+age = age + 1
+
+print(age)
+
+
+let closeTime = 5
+
+let salary = closeTime * 30
+
+let finalSalary =  salary - 20
+
+print(finalSalary)
+
+let lagestInteger = Int.max
+
+let smallestInteger = Int.min
+
+let unsignedIntegerMax = UInt.max
+
+print(lagestInteger)
+
+print(smallestInteger)
+
+print(unsignedIntegerMax)
+
+let smallIntValueSigned_8Bit = Int8.min
+let largestIntValueSigned_8Bit = Int8.max
+
+let largestIntValue_UnSigned_8Bit = UInt8.max
+let smallIntValue_UnSigned_8Bit = UInt8.min
+
+print("signed 8bit \(smallIntValueSigned_8Bit) ")
+print("signed 8bit \(largestIntValueSigned_8Bit)" )
+
+print("Unsigned 8bit \(smallIntValue_UnSigned_8Bit) ")
+print("Unsigned 8bit \(largestIntValue_UnSigned_8Bit)" )
+
+
+let lenghtofDress: Float = 25.7
+
+print(lenghtofDress)
+
+// Booleans true or false values
+
+var isHemarried = true
+
+var orisHeMarried = false
+
+let isPasswordCorrect: Bool = false
+
+
+var name = "Zainab"
+
+var fristLetter: Character = "Z"
+
+
+print("my name is \(name)") // String Interpolation is how to pass a variable into a string
+
+
+
+
+
+
+
+
+
+
